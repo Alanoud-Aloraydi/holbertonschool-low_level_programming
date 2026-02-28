971480f1ca70d19bf0f5664e3d97ae2bf81f8a4b
Alanoud Aloraydi <alanoudkh16@gmail.com>
2026-02-28 21:55:49 +0300

    Update 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ */
 void print_alphabet_x10(void)
 {
 	int i;
@@ -8,9 +11,7 @@ void print_alphabet_x10(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
-		{
 			_putchar(c);
-		}
 		_putchar('\n');
 	}
 }
