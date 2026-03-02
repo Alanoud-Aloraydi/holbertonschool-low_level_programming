556b37f0f4350c685bc5c7633df3ad75b1f44b7d
Alanoud_Aloraydi <alanoudkh16@gmail.com>
2026-03-02 21:51:36 +0000

    Betty: document _isupper and add README

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,14 @@
 #include "main.h"
 
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
-    return (c >= 'A' && c <= 'Z');
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
 }
