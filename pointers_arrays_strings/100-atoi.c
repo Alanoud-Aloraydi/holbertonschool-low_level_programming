fcc1307af8f7c86c8d44c1ffe75d282be51e8b1f
Alanoud_Aloraydi <alanoudkh16@gmail.com>
2026-03-05 20:46:29 +0000

    Refactor _atoi without brackets and avoid overflow

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,49 +9,39 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, started = 0;
+	char *p = s;
+	int sign = 1;
 	int num = 0;
 
-	while (s[i] != '\0')
+	while (*p != '\0' && (*p < '0' || *p > '9'))
 	{
-		if (!started)
-		{
-			if (s[i] == '-')
-				sign *= -1;
-			else if (s[i] >= '0' && s[i] <= '9')
-				started = 1;
-		}
-
-		if (started)
-		{
-			if (s[i] >= '0' && s[i] <= '9')
-			{
-				int digit = s[i] - '0';
-
-				if (sign == 1)
-				{
-					if (num > (INT_MAX - digit) / 10)
-						return (INT_MAX);
-					num = num * 10 + digit;
-				}
-				else
-				{
-					if (num < (INT_MIN + digit) / 10)
-						return (INT_MIN);
-					num = num * 10 - digit;
-				}
-			}
-			else
-			{
-				break;
-			}
-		}
-
-		i++;
+		if (*p == '-')
+			sign *= -1;
+		p++;
 	}
 
-	if (!started)
+	if (*p == '\0')
 		return (0);
+
+	while (*p >= '0' && *p <= '9')
+	{
+		int digit = *p - '0';
+
+		if (sign == 1)
+		{
+			if (num > (INT_MAX - digit) / 10)
+				return (INT_MAX);
+			num = num * 10 + digit;
+		}
+		else
+		{
+			if (num < (INT_MIN + digit) / 10)
+				return (INT_MIN);
+			num = num * 10 - digit;
+		}
+
+		p++;
+	}
 
 	return (num);
 }
