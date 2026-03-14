7cdd374b22ee6bf28979ec0cf80ebab5a63ce7b7
Alanoud_Aloraydi <alanoudkh16@gmail.com>
2026-03-14 19:07:33 +0000

    Fix betty style for return statements

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -7,13 +7,13 @@
  */
 int helper(int n, int i)
 {
-	if (i * i > n)
+	if ((i * i) > n)
 		return (-1);
 
-	if (i * i == n)
+	if ((i * i) == n)
 		return (i);
 
-	return helper(n, i + 1);
+	return (helper(n, i + 1));
 }
 
 /**
@@ -27,5 +27,5 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 
-	return helper(n, 0);
+	return (helper(n, 0));
 }
