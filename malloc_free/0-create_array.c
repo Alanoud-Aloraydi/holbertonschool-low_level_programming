46f0ef4466654df9889076343d5db2d413265eae
Alanoud Aloraydi <alanoudkh16@gmail.com>
2026-03-29 16:26:19 +0300

    Fix malloc size calculation in create_array function

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdlib.h>
 
 char *create_array(unsigned int size, char c)
@@ -9,7 +8,7 @@ char *create_array(unsigned int size, char c)
     if (size == 0)
         return (NULL);
 
-    arr = malloc(size);
+    arr = malloc(sizeof(char) * size);
     if (arr == NULL)
         return (NULL);
 
