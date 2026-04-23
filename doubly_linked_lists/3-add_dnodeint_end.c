7e687f745eb05758e77c7b0525ec3b811b0d6b14
Alanoud Aloraydi <alanoudkh16@gmail.com>
2026-04-23 14:56:22 +0000

    Add insert_dnodeint_at_index function

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,6 +13,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new;
 	dlistint_t *temp;
 
+	if (head == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
