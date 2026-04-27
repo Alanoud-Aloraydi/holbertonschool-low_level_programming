007fb036b8511620f89f5bb9c9f1e141ac4d85a6
Alanoud_Aloraydi <alanoudkh16@gmail.com>
2026-04-27 19:42:22 +0000

    fix: correct include guard in header file

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -5,10 +5,9 @@
 
 /**
  * struct hash_node_s - Node of a hash table
- *
  * @key: The key, string
  * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
+ * @next: A pointer to the next node
  */
 typedef struct hash_node_s
 {
@@ -19,9 +18,8 @@ typedef struct hash_node_s
 
 /**
  * struct hash_table_s - Hash table data structure
- *
  * @size: The size of the array
- * @array: An array of size @size
+ * @array: An array of linked lists
  */
 typedef struct hash_table_s
 {
@@ -29,18 +27,15 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/* Prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
-
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key,
+	unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key,
+	const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
-
 void hash_table_print(const hash_table_t *ht);
-
 void hash_table_delete(hash_table_t *ht);
 
-#endif
-
-unsigned long int hash_djb2(const unsigned char *str);
+#endif /* HASH_TABLES_H */
