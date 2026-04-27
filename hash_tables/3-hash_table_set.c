ec6773677dadcfecae88e16bb6017a6d3fef07b8
Alanoud_Aloraydi <alanoudkh16@gmail.com>
2026-04-27 19:02:21 +0000

    Task 3: add element to hash table

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,57 @@
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht: The hash table
+ * @key: The key
+ * @value: The value
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	unsigned long int index;
+	char *val_copy;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	node = ht->array[index];
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			val_copy = strdup(value);
+			if (val_copy == NULL)
+				return (0);
+
+			free(node->value);
+			node->value = val_copy;
+			return (1);
+		}
+		node = node->next;
+	}
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+	if (node->key == NULL || node->value == NULL)
+	{
+		free(node->key);
+		free(node->value);
+		free(node);
+		return (0);
+	}
+
+	node->next = ht->array[index];
+	ht->array[index] = node;
+
+	return (1);
+}
